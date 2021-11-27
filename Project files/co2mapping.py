5db33e8d8c2d02839a9b516666f083866b833274
Hamzah Shabbir <57156954+hamzahshabbir96@users.noreply.github.com>
2021-11-27 17:30:40 +0100

    Update co2mapping.py

--- Project files/co2mapping.py
+++ Project files/co2mapping.py
@@ -5,13 +5,9 @@ Created on Sat Nov 27 13:08:14 2021
 @author: Hamzah
 """
 
-import pandas as pd
-d={'Type':['Benzin','Diesel','Natural gas','Electric','Bus(long route)','Bus(short route)','Flight','Tram','Subway'],
-   'Emission':[135,124,116,90,29,83,214,90,54]}
-database_fuel=pd.DataFrame(data=d)
-database_fuel=database_fuel.set_index('Type')
-def co2_mapping(list_of_routes,mode="transit",driving_fuel="Diesel"):
-    if mode== "driving":
+def co2_mapping(list_of_routes,mode="driving",driving_fuel="Diesel"):
+    
+    if mode=="driving":
         updated_routes=[]
         
         for i in range(len(list_of_routes)):
@@ -30,7 +26,7 @@ def co2_mapping(list_of_routes,mode="transit",driving_fuel="Diesel"):
             route_update['CO2 Emission']=temp
             updated_routes.append(route_update)
         
-    if mode == "transit":
+    elif mode == "transit":
         updated_routes=[]
         
         for i in range(len(list_of_routes)):
@@ -42,9 +38,33 @@ def co2_mapping(list_of_routes,mode="transit",driving_fuel="Diesel"):
                 elif(route.iloc[i]['Name of Public Transport'] == "Bus"):
                     temp.append((route.iloc[i]['Distance'])*83/1000)
                 elif(route.iloc[i]['Name of Public Transport'] == "Tram"):
-                    temp.append((route.iloc[i]['Distance'])*90/1000)
+                    temp.append((route.iloc[i]['Distance'])*54/1000)
                 else:
                     temp.append(0)
+            route_update=route.copy()
+            route_update['CO2 Emission']=temp
+            updated_routes.append(route_update)
+    elif mode == "bicycling":
+        updated_routes=[]
+        
+        for i in range(len(list_of_routes)):
+            route=list_of_routes[i]
+            temp=[]
+            for i in range(len(route)):
+                
+                temp.append(0)
+            route_update=route.copy()
+            route_update['CO2 Emission']=temp
+            updated_routes.append(route_update)
+    elif mode == "walking":
+        updated_routes=[]
+        
+        for i in range(len(list_of_routes)):
+            route=list_of_routes[i]
+            temp=[]
+            for i in range(len(route)):
+                
+                temp.append(0)
             route_update=route.copy()
             route_update['CO2 Emission']=temp
             updated_routes.append(route_update)
