ff2460adb1f831899f78767131e775ca1a4edd62
Hamzah Shabbir <57156954+hamzahshabbir96@users.noreply.github.com>
2021-11-27 17:32:13 +0100

    Create main.py

---
+++ Project files/main.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 27 15:52:05 2021
+
+@author: Hamzah
+"""
+
+
+def find_routes(origin="Nymphenburg Palace",destination="Marienplatz",departure_time="1638196470",driving_fuel="Diesel"):
+    import requests
+    import pandas as pd
+    modes=["driving","transit","bicycling","walking"]
+    dict={}
+    dict1={}
+    #temp=[]
+    for i in modes:
+        #temp.append(get_routes(origin, destination,i, departure_time))
+        dict[i]=get_routes(origin,destination,i,departure_time)
+    for i in modes:
+        dict1[i]=co2_mapping(dict[i],i,driving_fuel)
+        
+    return dict1
